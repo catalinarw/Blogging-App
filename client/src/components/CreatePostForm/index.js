b66b545ee9951881f25a8857fca3cbaadbaaeb3a
Catalina Williams <catalina.rose1361@gmail.com>
2021-02-05 15:01:28 -0700

    fixed create post form page

--- client/src/components/CreatePostForm/index.js
+++ client/src/components/CreatePostForm/index.js
@@ -1,43 +1,29 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import Jumbotron from "../components/Jumbotron";
-import { Col, Row, Container } from "../components/Grid";
 
-function Detail(props) {
+const CreatePostForm = () => {
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
+
   return (
-    <>{/* Replace `true` with the state of your application */}{true ? (
-      <Container fluid>
-        <Row>
-          <Col size="md-12">
-            <Jumbotron>
-              <h1>TITLE by AUTHOR</h1>
-            </Jumbotron>
-          </Col>
-        </Row>
-        <Row>
-          <Col size="md-10 md-offset-1">
-            <article>
-              <h1>Content:</h1>
-              <p>BODY</p>
-            </article>
-          </Col>
-          {/* Replace `false` to check if the current post is in the favorites list */}
-          {false ? (
-            <button className="btn btn-danger">Remove from Favorites!</button>
-          ) : (
-            <button className="btn">❤️ Add to Favorites</button>
-          )}
-        </Row>
-        <Row>
-          <Col size="md-2">
-            <Link to="/">← Back to Posts</Link>
-          </Col>
-        </Row>
-      </Container>
-    ) : (
-      <div>loading...</div>
-    )}</>
+    <div>
+      <div className="jumbotron">
+        <img
+          className="img-fluid img-thumbnail"
+          src="https://images.pexels.com/photos/459688/pexels-photo-459688.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        />
+      </div>
+      <h1>Create a blog post</h1>
+      <form className="form-group mt-5 mb-5" onSubmit={handleSubmit}>
+        <input className="form-control mb-5" required placeholder="Title" />
+        <textarea className="form-control mb-5" required placeholder="Body" />
+        <input className="form-control mb-5" placeholder="Screen name" />
+        <button className="btn btn-success mt-3 mb-5" type="submit">
+          Save Post
+        </button>
+      </form>
+    </div>
   );
-}
+};
 
-export default Detail;
+export default CreatePostForm;
