dbed24e52352a7a48046224c6f1996ef83998c81
Catalina Williams <catalina.rose1361@gmail.com>
2021-02-05 15:30:40 -0700

    wrapped application in store provider

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,23 @@ import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
+import { StoreProvider } from "./utils/GlobalState";
 import FavoritesList from "./pages/FavoritesList";
 
 function App() {
   return (
     <Router>
       <div>
-        <Nav />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/home" component={Home} />
-          <Route exact path="/favorites" component={FavoritesList} />
-          <Route exact path="/posts/:id" component={Detail} />
-          <Route component={NoMatch} />
-        </Switch>
+        <StoreProvider>
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/home" component={Home} />
+            <Route exact path="/favorites" component={FavoritesList} />
+            <Route exact path="/posts/:id" component={Detail} />
+            <Route component={NoMatch} />
+          </Switch>
+        </StoreProvider>
       </div>
     </Router>
   );
