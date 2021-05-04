a5ce7f6b4009618beaf9b23bace7dd0c6de646f9
Catalina Williams <webdevcatalina@gmail.com>
2021-05-04 15:45:58 -0600

    removed unused code from server.js

--- server.js
+++ server.js
@@ -26,30 +26,3 @@ if(process.env.NODE_ENV === 'production') {
 app.listen(port, function() {
     console.log("express server is running")
 })
-
-// const express = require("express");
-
-// const mongoose = require("mongoose");
-// const routes = require("./routes");
-// const app = express();
-// const PORT = process.env.PORT || 3001;
-
-// // Define middleware here
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-// // Serve up static assets (usually on heroku)
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(__dirname, 'build', 'index.html'));
-// }
-// // Add routes, both API and view
-// app.use(routes);
-
-// // Connect to the Mongo DB
-// const connection =  "mongodb+srv://catalina-admin:fliaJUchyWudg52g@cluster0.xttdr.mongodb.net/blogPostsDB";
-// mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
-//     .then(() => console.log("Database Connected Successfully"))
-//     .catch(err => console.log(err));
-// // Start the API server
-// app.listen(PORT, function() {
-//   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-// });
